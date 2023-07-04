bd6a9cb2eda9718554697b6949613e7b6d350eab
cany <couleurvide1008@gmail.com>
2023-07-04 21:01:44 +0900

    できること欄にアイコンを追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./reset.css";
 import "./App.scss";
-import { IoIosArrowDown, IoIosArrowDropupCircle } from "react-icons/io";
+import { IoIosArrowDropupCircle } from "react-icons/io";
+import { FaAngleDown, FaHtml5, FaSass, FaReact } from "react-icons/fa";
+import { SiAdobephotoshop, SiAdobeillustrator } from "react-icons/si";
 
 function App() {
   const returnTop = () => {
@@ -32,38 +34,45 @@ function App() {
         </div>
         <div className="scroll">
           <p>Scroll</p>
-          <IoIosArrowDown
+          <FaAngleDown
             className="scroll-icon"
             size={"30px"}
             color={"#fff"}
+            opacity={"0.8"}
           />
         </div>
       </header>
       <div className="main">
-        <div className="container about">
+        <div className="main-container about">
           <h2>About</h2>
           <div className="about-inner">
             <img className="portrait" src="./photo.jpg" alt="" />
             <div className="introduce">
               <div className="career">
                 <h3>堂寺ユミ（どうてらゆみ）</h3>
-                <p>2019年から某区役所で職員として勤務。</p>
-                <p>情報部門や人事部門で業務に従事してきました。</p>
+                <p>2019年から某23区役所で職員として勤務。</p>
+                <p>情報部門や人事部門の業務に従事してきました。</p>
                 <p>プログラミングは独学です。</p>
               </div>
               <div className="introduce-inner skills">
                 <h3>できること</h3>
-                <p>HTML、CSS、JavaScript（React）</p>
+                <div className="skill-icons">
+                  <FaHtml5 size={"30px"} />
+                  <FaSass size={"30px"} />
+                  <FaReact size={"30px"} />
+                  <SiAdobephotoshop size={"30px"} />
+                  <SiAdobeillustrator size={"30px"} />
+                </div>
               </div>
               <div className="introduce-inner qualifications">
                 <h3>合格歴</h3>
-                <p>ITパスポート</p>
-                <p>情報セキュリティマネジメント試験</p>
+                <p>ITパスポート(2021)</p>
+                <p>情報セキュリティマネジメント試験(2022)</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="container works">
+        <div className="main-container works">
           <h2>Works</h2>
           <div className="works-inner">
             <div className="item">1</div>
@@ -78,7 +87,7 @@ function App() {
           <p>Page top</p>
           <IoIosArrowDropupCircle size={"50px"} />
         </div>
-        <div className="container contact">
+        <div className="main-container contact">
           <h2>Contact</h2>
           <div className="contact-inner"></div>
         </div>
