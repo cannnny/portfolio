1bd63a335b27d491055433a9603037ccdca303a9
cany <couleurvide1008@gmail.com>
2023-09-25 22:56:21 +0900

    stylelintのルールを追加

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,5 +5,8 @@ module.exports = {
   rules: {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
+    "color-hex-length": null,
+    "selector-list-comma-newline-after": "never-multi-line",
+    "selector-list-comma-space-after": "always-single-line",
   },
 };
