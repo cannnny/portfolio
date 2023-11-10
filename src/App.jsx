5ec482664ee4f0db2a92233d3c1f37675208e9a9
cany <couleurvide1008@gmail.com>
2023-11-10 17:45:41 +0900

    制作物のデータを追加、worksDataを別ファイルに分割

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import "./reset.css";
 import "./App.scss";
 
+// 制作物データ
+import { worksData } from "./worksData";
+
 // コンポーネント
 import MainVisual from "./components/MainVisual";
 import About from "./components/About";
@@ -84,57 +87,65 @@ function App() {
   }, []);
 
   // 掲載している制作物一覧
-  const worksData = [
-    {
-      name: "プロフィールサイト",
-      url: "https://reproduct-cannnny.vercel.app/",
-      img: "thumb-pf.webp",
-      category: "模写",
-      tag: ["HTML&CSS"],
-      overview: "プログラミング学習の最初に制作した模写サイトです。",
-    },
-    {
-      name: "モンハン弱点教えるくん",
-      url: "https://mh-weak-info.vercel.app/",
-      img: "thumb-mh.webp",
-      category: "開発",
-      tag: ["React", "microCMS"],
-      overview:
-        "「モンスターハンター」に登場するモンスターの弱点を検索できるWebアプリです。",
-    },
-    {
-      name: "Hello Pawmo!",
-      url: "https://hello-pawmo.vercel.app/",
-      img: "thumb-hp.webp",
-      category: "開発",
-      tag: ["React"],
-      overview: "「パモット」がメッセージに答えて挨拶してくれるWebアプリです。",
-    },
-    {
-      name: "らぶぱも",
-      url: "https://love-pawmi.vercel.app/",
-      img: "thumb-lp.webp",
-      category: "開発",
-      tag: ["React", "Photoshop"],
-      overview: "「パモ」のぬいぐるみをひたすら眺めるWebアプリです。",
-    },
-    {
-      name: "Canipture!",
-      url: "https://canipture.vercel.app/",
-      img: "thumb-ca.webp",
-      category: "開発",
-      tag: ["React", "microCMS", "Swiper", "Illustrator"],
-      overview: "ゲームのスクリーンショットを掲載したギャラリーです。",
-    },
-    {
-      name: "きょう何食べる？",
-      url: "https://whats-for-dinner-etjbd6z2s-cannnny.vercel.app/",
-      img: "thumb-wd.webp",
-      category: "開発",
-      tag: ["React", "microCMS", "Framer Motion", "Jotai", "Illustrator"],
-      overview: "気分に合わせて今日のご飯を提案してくれるWebアプリです。",
-    },
-  ];
+  // const worksData = [
+  //   {
+  //     name: "プロフィールサイト",
+  //     url: "https://reproduct-cannnny.vercel.app/",
+  //     img: "thumb-pf.webp",
+  //     category: "模写",
+  //     tag: ["HTML&CSS"],
+  //     overview: "プログラミング学習の最初に制作した模写サイトです。",
+  //   },
+  //   {
+  //     name: "モンハン弱点教えるくん",
+  //     url: "https://mh-weak-info.vercel.app/",
+  //     img: "thumb-mh.webp",
+  //     category: "開発",
+  //     tag: ["React", "microCMS"],
+  //     overview:
+  //       "「モンスターハンター」に登場するモンスターの弱点を検索できるWebアプリです。",
+  //   },
+  //   {
+  //     name: "Hello Pawmo!",
+  //     url: "https://hello-pawmo.vercel.app/",
+  //     img: "thumb-hp.webp",
+  //     category: "開発",
+  //     tag: ["React"],
+  //     overview: "「パモット」がメッセージに答えて挨拶してくれるWebアプリです。",
+  //   },
+  //   {
+  //     name: "らぶぱも",
+  //     url: "https://love-pawmi.vercel.app/",
+  //     img: "thumb-lp.webp",
+  //     category: "開発",
+  //     tag: ["React", "Photoshop"],
+  //     overview: "「パモ」のぬいぐるみをひたすら眺めるWebアプリです。",
+  //   },
+  //   {
+  //     name: "Canipture!",
+  //     url: "https://canipture.vercel.app/",
+  //     img: "thumb-ca.webp",
+  //     category: "開発",
+  //     tag: ["React", "microCMS", "Swiper", "Illustrator"],
+  //     overview: "ゲームのスクリーンショットを掲載したギャラリーです。",
+  //   },
+  //   {
+  //     name: "きょう何食べる？",
+  //     url: "https://whats-for-dinner-etjbd6z2s-cannnny.vercel.app/",
+  //     img: "thumb-wd.webp",
+  //     category: "開発",
+  //     tag: ["React", "microCMS", "Framer Motion", "Jotai", "Illustrator"],
+  //     overview: "気分に合わせて今日のご飯を提案してくれるWebアプリです。",
+  //   },
+  //   {
+  //     name: "【制作中】ランディングページ",
+  //     url: "https://rp-bakery.vercel.app/",
+  //     img: "thumb-rp.webp",
+  //     category: "模写",
+  //     tag: ["HTML&CSS", "JavaScript", "Photoshop", "Illustrator"],
+  //     overview: "架空のパン屋のWebサイトとして制作した模写サイトです。",
+  //   },
+  // ];
 
   return (
     <div className="App">
